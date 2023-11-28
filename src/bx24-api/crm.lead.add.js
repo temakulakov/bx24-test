86a29ee05c4b98868412b63c35d9e0767f1c36c2
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2023-11-28 15:23:38 +0300

    init

--- src/bx24-api/crm.lead.add.js
+++ src/bx24-api/crm.lead.add.js
@@ -7,8 +7,7 @@ import {headRowText, leftColText} from "../store/data";
     const description = answers.map((el, ind) => leftColText[ind] + " - " + headRowText[el] + "    \n").join('');
     console.log(description);
     const contactsCreateRequestString = "https://gagawa.bitrix24.ru/rest/9/imsk3z7t8hjyhtc2/crm.contact.add?" +
-        `fields[STATUS_ID]=${"NEW"}&fields[NAME]=${lastName !== undefined ? secondName + " " + name + " " + lastName : name + " " + secondName}&fields[POST]=${state}` +
-    `&fields[UF_CRM_1699599951990]=${answers[0]}&fields[UF_CRM_1699599977064]=${answers[1]}&fields[UF_CRM_1699599999778]=${answers[2]}&fields[UF_CRM_1699600032795]=${answers[3]}&fields[UF_CRM_1699600054919]=${answers[4]}&fields[UF_CRM_1699600072362]=${answers[5]}`;
+        `fields[STATUS_ID]=${"NEW"}&fields[NAME]=${lastName !== undefined ? secondName + " " + name + " " + lastName : name + " " + secondName}&fields[POST]=${state}`;
     const contactsCreateRequestStringGCTM = `https://intranet.gctm.ru/rest/1552/sqjno5st2nedtafg/crm.contact.add?` +
         `fields[STATUS_ID]=${"NEW"}&fields[NAME]=${lastName !== undefined ? secondName + " " + name + " " + lastName : name + " " + secondName}&fields[POST]=${state}`;
      let contactId;
@@ -19,9 +18,8 @@ import {headRowText, leftColText} from "../store/data";
      console.log("contactId");
      console.log(contactId);
      const dealCreateRequestStringGCTM = `https://intranet.gctm.ru/rest/1552/sqjno5st2nedtafg/crm.deal.add?fields[CONTACT_ID]=${contactId}` +
-         `&fields[TITLE]=${name + " " + secondName + " " + 'прошел тест'}`;
-
-     const dealCreateRequestString = `https://gagawa.bitrix24.ru/rest/9/imsk3z7t8hjyhtc2/crm.deal.add?fields[CONTACT_ID]=${contactId}` +
+         `&fields[TITLE]=${name + " " + secondName + " " + 'прошел тест'}\`&fields[UF_CRM_1699599951990]=${answers[0]}&fields[UF_CRM_1699599977064]=${answers[1]}&fields[UF_CRM_1699599999778]=${answers[2]}&fields[UF_CRM_1699600032795]=${answers[3]}&fields[UF_CRM_1699600054919]=${answers[4]}&fields[UF_CRM_1699600072362]=${answers[5]}\``;
+     const dealCreateRequestString = `https://gagawa.bitrix24.ru/rest/9/imsk3z7t8hjyhtc2/crm.deal.add?fields[CONTACT_ID]=${contactId}\`&fields[UF_CRM_1699599951990]=${answers[0]}&fields[UF_CRM_1699599977064]=${answers[1]}&fields[UF_CRM_1699599999778]=${answers[2]}&fields[UF_CRM_1699600032795]=${answers[3]}&fields[UF_CRM_1699600054919]=${answers[4]}&fields[UF_CRM_1699600072362]=${answers[5]}\`` +
          `&fields[TITLE]=${name + " " + secondName + " " + 'прошел тест'}`;
 
      await axios.post(dealCreateRequestString).then((resp) => {
